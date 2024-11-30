fa38fac695c2eced8e41f50e19b8e565602004f8
Kou-ding <fotispapadak@gmail.com>
2024-11-30 03:54:54 +0200

    injection attempts and er final graph

---
+++ SQL-Injection/injection.sh
@@ -0,0 +1,9 @@
+curl -X 'POST'   'https://issel10.ee.auth.gr/token/'\
+  -H 'accept: application/json'\
+  -H 'Content-Type: application/json'\
+  -d '{
+        "username": "foxy",
+        "password": "freddys",
+        "email": "fnaf@mail.com",
+        "address": "fnaf street"
+}'
