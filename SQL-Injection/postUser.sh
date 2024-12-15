d9bf93b772f9e8626eb28b615dcf61055db40f6a
Kou-ding <fotispapadak@gmail.com>
2024-12-16 00:06:58 +0200

    divide the laboratory exercises from the course's project and the sql injection sanbox

--- SQL-Injection/postUser.sh
+++
@@ -1,9 +0,0 @@
-curl -X 'POST'   'https://issel10.ee.auth.gr/token/'\
-  -H 'accept: application/json'\
-  -H 'Content-Type: application/json'\
-  -d '{
-        "username": "testname",
-        "password": "testpass",
-        "email": "testing@mail.com",
-        "address": "World"
-}'
